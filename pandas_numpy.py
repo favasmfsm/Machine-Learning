5366b04e32663e84bb971cabbc441cd089fcf962
favasmfsm <favasmfsm@gmail.com>
2021-01-02 23:33:27 +0530

    hackerearth

---
+++ pandas_numpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+L=list(range(10))
+
+str_L=[str(c) for c in L]
+#print(str_L) #prints the value 0 to 9
+
+L2=np.zeros(10, dtype=int)
+#print(L2) #print the array of zeroes
+
+L3=np.ones((3,5,6), dtype=int)
+#print(L3) #prints the array of defined size of ones
+
+#print(np.full((5,6,7),1.23))
+
+#print(np.arange(0,20,2))
+
+#print(np.linspace(0,20,10))
+
+print(np.random.normal(0,2,(3,3)))
